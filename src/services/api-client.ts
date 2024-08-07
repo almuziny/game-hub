a7e111711bbfb93d2692fe5caed4dbe4d8d73b73
almuziny <ma.almuzainy@gmail.com>
2024-08-07 10:24:52 +0300

    Change the RAWG API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "63ba4c113dee4e859a4465038b5e9bd5",
   },
 });
