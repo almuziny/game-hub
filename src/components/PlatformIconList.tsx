b958b74c3d26fcb51df378eef70a6568f131d4c7
almuziny <ma.almuzainy@gmail.com>
2024-09-01 08:35:40 +0300

    Exercise - Refactoring Entities

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,17 +1,13 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
 import {
-  FaWindows,
-  FaPlaystation,
-  FaXbox,
-  FaApple,
-  FaLinux,
-  FaAndroid,
+  FaAndroid, FaApple,
+  FaLinux, FaPlaystation, FaWindows, FaXbox
 } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
-import { BsGlobe } from "react-icons/bs";
-import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../entities/Platform";
-import { IconType } from "react-icons";
+import Platform from "../entities/Platform";
 
 interface Props {
   platforms: Platform[];
--- src/entities/Publisher.ts
+++ src/entities/Publisher.ts
@@ -1,4 +1,4 @@
-export interface Publishers {
-    id: number;
-    name: string;
+export default interface Publishers {
+  id: number;
+  name: string;
 }
